45f29091a6166d212f53e633ed6b54e981c7efb2
Ajith P <ajith.aju39502@gmail.com>
2026-03-18 19:05:18 +0530

    .

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,10 +3,15 @@ import 'expo-sqlite/localStorage/install';
 import { Database } from './database.types';
 
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://lommrxllbefhqzhadfmx.supabase.co';
-const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_KEY || 'sb_publishable_ctFnhg30ouuO4TZyB1EyVw_LIgjMSoy';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL?.startsWith('https://')
+    ? process.env.EXPO_PUBLIC_SUPABASE_URL
+    : 'https://lommrxllbefhqzhadfmx.supabase.co';
 
-console.log('SUPABASE URL:', process.env.EXPO_PUBLIC_SUPABASE_URL)
+const supabasePublishableKey = process.env.EXPO_PUBLIC_SUPABASE_KEY?.startsWith('sb_')
+    ? process.env.EXPO_PUBLIC_SUPABASE_KEY
+    : 'sb_publishable_ctFnhg30ouuO4TZyB1EyVw_LIgjMSoy';
+
+
 
 export const supabase = createClient<Database>(supabaseUrl, supabasePublishableKey, {
     auth: {
